b54d373a60a3b53781f1f91366345b2b48f2f8a4
Vihan Perera <47180113+hanzperera99@users.noreply.github.com>
2021-10-05 09:42:40 +0530

    Imgs and slider added

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,16 @@
 import React from "react";
+import Carousel from '../inc/Carousel'
 
 
 function Home() {
     return(
-        <div className="container">
-            <div className="card shadow mt-5">
-                <div className="card-body">
-                    <h2>Home Page</h2>
+        <div>
+            <Carousel />
+            <div className="container">
+                <div className="card shadow mt-5">
+                    <div className="card-body">
+                        <h2>Home Page</h2>
+                    </div>
                 </div>
             </div>
         </div>
